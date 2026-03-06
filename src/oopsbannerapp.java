9ee29d9d75ba7a5b4d623d83d1802cefc6c4c790
Akshaya22123 <akshayakotha22@gmail.com>
2026-03-06 21:00:12 +0530

    feature/uc4

--- src/oopsbannerapp.java
+++ src/oopsbannerapp.java
@@ -1,5 +1,5 @@
 class oopsbannerapp{
-    public class OOPSBannerApp {
+    
 
     public static void main(String[] args) {
         // 1. Define a String array with a size equal to the number of lines (7)
@@ -7,20 +7,20 @@ class oopsbannerapp{
 
         // 2. Populate each index of the array using String.join()
         // We join the patterns for 'O', 'O', 'P', and 'S' with a space separator
-        lines[0] = String.join("  ", "  *  ", "  *  ", " ** ", " ** ");
-        lines[1] = String.join("  ", "*   *", "*   *", "*   *", "**     ");
-        lines[2] = String.join("  ", "*   *", "*   *", "*   *", "**     ");
-        lines[3] = String.join("  ", "*   *", "*   *", " ** ", " ** ");
-        lines[4] = String.join("  ", "*   *", "*   *", "*     ", "     *");
-        lines[5] = String.join("  ", "*   *", "*   *", "*     ", "     *");
-        lines[6] = String.join("  ", "  *  ", "  *  ", "*     ", " ** ");
+        lines[0] = String.join("  ", " ***  ", " ***  ", "***** ", "  *****  ");
+        lines[1] = String.join("  ", "*   * ", "*   * ", "*   * ", " **      ");
+        lines[2] = String.join("  ", "*   * ", "*   * ", "*   * ", "**       ");
+        lines[3] = String.join("  ", "*   * ", "*   * ", "***** ", " *****   ");
+        lines[4] = String.join("  ", "*   * ", "*   * ", "*     ", "     **  ");
+        lines[5] = String.join("  ", "*   * ", "*   * ", "*     ", "**    ** ");
+        lines[6] = String.join("  ", " ***  ", " ***  ", "*     ", "  *****  ");
 
         // 3. Use a for-each loop to iterate through the array and print each line
         System.out.println("--- OOPS Banner ---");
         for (String line : lines) {
             System.out.println(line);
     }
-}
+
 }
 }
 
